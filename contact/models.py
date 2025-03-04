c35401f787102cf85c8c829dd1dea7363dd38345
Francisco Gledyson <gledysoncruz@gmail.com>
2025-03-04 13:37:09 -0300

    Registrando meu model na área administrativa do Django (admin.py e register)

--- contact/models.py
+++ contact/models.py
@@ -8,3 +8,6 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     create_date =models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.first_name
