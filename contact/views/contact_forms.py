6708afaed23e5cf0424349f3b3316832aff45ea1
Francisco Gledyson <gledysoncruz@gmail.com>
2025-03-08 14:02:12 -0300

    Usando instance do ModelForm para atualizar dados de um contato

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 from contact.models import Contact
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
+    else:
+        form = ContactForm()
 
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:index')
+
         return render(request, 'contact/create.html', context)
