fe9bf0dd2ae6aaf3fe9a8fe357d30fea8b9e1e98
Francisco Gledyson <gledysoncruz@gmail.com>
2025-03-08 10:29:32 -0300

    HTML e CSS da nossa página create.html

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.db.models import Q
+from contact.models import Contact
+
+def create(request):
+    return render(request, 'contact/create.html',)
